84ccb2b3ec397348d65c9f74f5066b367c3fa88a
MahmoudDawood <mm.dawood16@yahoo.com>
2023-09-06 14:20:05 +0300

    Use vite and TypeScript

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-const App = () => {
-  return <header>Data School Frontend</header>;
-};
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Data School</h1>
+    </>
+  )
+}
+
+export default App
